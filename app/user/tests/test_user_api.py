cf8e450a437db8b5117bb9b8fa0bcda93f67f4ea
Chidera Mosanya <camosanya@gmail.com>
2022-12-11 22:43:24 +0100

    added user model and api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,7 +12,7 @@ CREATE_USER_URL = reverse("user:create")
 
 def create_user(**params):
     """Create and return a new user"""
-    return get_user_model().objects.crete_user(**params)
+    return get_user_model().objects.create_user(**params)
 
 
 class PublicUserAPITest(TestCase):
@@ -30,7 +30,7 @@ class PublicUserAPITest(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
-        user = get_user_model().objects.get(email=payload('email'))
+        user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
@@ -58,5 +58,6 @@ class PublicUserAPITest(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']).exists()
         self.assertFalse(user_exists)
