a2da0b27a2d79c327e2c8b3973b9a4365880ff33
Chidera Mosanya <camosanya@gmail.com>
2022-12-14 00:02:00 +0100

    added recipe serializer

---
+++ app/recipe/serializers.py
@@ -0,0 +1,13 @@
+"""Serializers for Recipe API"""
+
+from rest_framework import serializers
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializers for Recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', "price", "link"]
+        read_only_fields = ['id']
