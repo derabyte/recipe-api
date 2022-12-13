26475efe6dc8a72ff385fa7b767a71d59628a313
Chidera Mosanya <camosanya@gmail.com>
2022-12-13 20:07:05 +0100

    create recipe app

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
 """Database models"""
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -41,4 +42,18 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """Recipe Objects"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
